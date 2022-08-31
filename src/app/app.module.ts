eeca781e7794d98a23924fbd4da1d1b2eaa9295b
buchholzm <109744077+buchholzm@users.noreply.github.com>
2022-08-31 23:58:39 +0200

    Extract header to seperate component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './core/components/app-header/app-header.component';
 
 @NgModule({
   // Components, directives and pipes
-  declarations: [AppComponent],
+  declarations: [AppComponent, AppHeaderComponent],
   // Bootstrap components
   bootstrap: [AppComponent],
   // Services provided
   providers: [],
   // Imported modules (with declarations and providers)
-  imports: [BrowserModule]
+  imports: [BrowserModule],
 })
 export class AppModule {}
