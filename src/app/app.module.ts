c0515e99cce61a2bf885a293be608f8fa72f2ee0
buchholzm <109744077+buchholzm@users.noreply.github.com>
2022-08-31 21:23:27 +0200

    Add app module that declares the component.

---
+++ src/app/app.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  // Components, directives and pipes
+  declarations: [AppComponent],
+})
+export class AppModule {}
