75b17bc8258073d57207d829e77946aa74ac6ddb
buchholzm <109744077+buchholzm@users.noreply.github.com>
2022-09-02 14:55:24 +0200

    Provide invalid link and configure a catch-all route.

--- src/app/core/core.routes.ts
+++ src/app/core/core.routes.ts
@@ -1,6 +1,12 @@
 import { Route } from '@angular/router';
 import { HomePage } from './pages/home/home.component';
+import { NotFoundPage } from './pages/not-found/not-found.component';
 
 export const coreRoutes: Route[] = [
   { path: '', component: HomePage, title: '' },
+  {
+    path: '**',
+    component: NotFoundPage,
+    title: $localize`:@@notFoundTitle:Oops`,
+  },
 ];
