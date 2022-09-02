6fada797bb337f6becca2954c2c11e2d36aff3d6
buchholzm <109744077+buchholzm@users.noreply.github.com>
2022-09-02 21:48:29 +0200

    Route imperatively after five seconds to the homepage.

--- src/app/core/pages/not-found/not-found.component.ts
+++ src/app/core/pages/not-found/not-found.component.ts
@@ -1,6 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './not-found.component.html',
 })
-export class NotFoundPage {}
+export class NotFoundPage implements OnInit, OnDestroy {
+  countdown = 5;
+  countdownDots = Array(this.countdown).map((_, ix) => ix + 1);
+
+  constructor(private router: Router) {}
+
+  countdownTimer: number = 0;
+  ngOnInit(): void {
+    this.countdownTimer = window.setInterval(() => {
+      --this.countdown;
+      if (this.countdown === 0) {
+        clearInterval(this.countdownTimer);
+        this.router.navigateByUrl('/');
+      }
+    }, 1000);
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.countdownTimer);
+  }
+}
