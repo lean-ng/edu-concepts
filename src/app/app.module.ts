69cccd6c23c6ea7383101bb735abf3bb5cbaef57
buchholzm <109744077+buchholzm@users.noreply.github.com>
2022-09-02 20:47:53 +0200

    Fix route order. And delete core module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-
-import { CoreModule } from './core/core.module';
+import { RouterModule, TitleStrategy } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './core/components/app-header/app-header.component';
+
+import { PageTitleService } from './core/services/page-title.service';
+
 import { appRoutes } from './app.routes';
 
 @NgModule({
   // Components, directives and pipes
-  declarations: [AppComponent],
+  declarations: [AppComponent, AppHeaderComponent],
   // Bootstrap components
   bootstrap: [AppComponent],
   // Services provided
-  providers: [],
+  providers: [{ provide: TitleStrategy, useClass: PageTitleService }],
   // Imported modules (with declarations and providers)
-  imports: [BrowserModule, CoreModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
 })
 export class AppModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Route } from '@angular/router';
 
+import { HomePage } from './core/pages/home/home.component';
+import { NotFoundPage } from './core/pages/not-found/not-found.component';
+
 export const appRoutes: Route[] = [
   // Lazy routed modules
   {
     path: 'timeline',
     loadChildren: () =>
       import('./timeline/timeline.module').then((m) => m.TimelineModule),
+  },
+  { path: '', component: HomePage, title: '' },
+  {
+    path: '**',
+    component: NotFoundPage,
+    title: $localize`:@@notFoundTitle:Oops`,
   },
 ];
